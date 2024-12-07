7d5f355f214af93e01311b272187dd5a789792dc
Parth Gupta <parth.eng1210@gmail.com>
2024-12-07 20:22:15 +0530

    style: minor changes

--- frontend/components/WalletSelectDialog.tsx
+++ frontend/components/WalletSelectDialog.tsx
@@ -27,13 +27,13 @@ export const WalletSelectionDialog = ({
 
   return (
     <div id="modal-overlay" className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50 font-[family-name:var(--font-geist-mono)]" onClick={handleOutsideClick}>
-      <div className="bg-black rounded-lg shadow-lg p-6 w-80">
+      <div className="bg-black border border-white rounded-xl shadow-lg p-6 w-80">
         <h2 className="text-xl font-semibold mb-4 text-center">
           Select a Wallet
         </h2>
         <div className="flex flex-col gap-4">
           <button
-            className="flex items-center gap-3 bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-lg transition-all"
+            className="flex items-center gap-3 border border-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-xl transition-all"
             onClick={() => {
               openWalletConnectModal();
               setOpen(false);
@@ -50,7 +50,7 @@ export const WalletSelectionDialog = ({
             WalletConnect
           </button>
           <button
-            className="flex items-center gap-3 bg-yellow-500 hover:bg-yellow-600 text-white px-4 py-2 rounded-lg transition-all"
+            className="flex items-center gap-3 border border-yellow-500 hover:bg-yellow-600 text-white px-4 py-2 rounded-xl transition-all"
             onClick={() => {
               connectToMetamask();
               setOpen(false);
