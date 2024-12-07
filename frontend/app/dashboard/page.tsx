d6a39f3569cf9a4423440fd8f06b2c2da40a85e1
Parth Gupta <parth.eng1210@gmail.com>
2024-12-07 16:13:04 +0530

    feat: add dashboard

---
+++ frontend/app/dashboard/page.tsx
@@ -0,0 +1,93 @@
+"use client";
+import Header from "@/components/Header";
+import { useState } from "react";
+
+export default function Dashboard() {
+  const [isAdminOpen, setIsAdminOpen] = useState(true);
+  const [isHolderOpen, setIsHolderOpen] = useState(true);
+
+  return (
+    <div className="py-6 px-10 min-h-screen font-[family-name:var(--font-geist-mono)]">
+      <Header />
+      <div>
+        <div className="flex justify-between items-center mb-6 mt-14">
+          <h1 className="text-2xl font-semibold">Dashboard</h1>
+          <div className="flex gap-4">
+            <button className="px-4 py-2 text-purple-100 border border-purple-700 rounded-xl">
+              + Import a digital security token
+            </button>
+            <button className="px-4 py-2 bg-purple-500 text-white rounded-xl hover:bg-purple-600">
+              Create new digital security
+            </button>
+          </div>
+        </div>
+
+        {/* Admin Section */}
+        <div className="mb-8 border border-white rounded-xl">
+          <div className="flex justify-between items-center p-6 bg-purple-500 rounded-xl">
+            <div
+              className="flex items-center cursor-pointer"
+              onClick={() => setIsAdminOpen(!isAdminOpen)}
+            >
+              <h2 className="text-lg font-medium mr-2">Admin</h2>
+              <span
+                className={`transform transition-transform ${
+                  isAdminOpen ? "rotate-180" : "rotate-0"
+                }`}
+              >
+                ▼
+              </span>
+            </div>
+            <button className="text-sm text-white hover:underline">
+              See all
+            </button>
+          </div>
+          {isAdminOpen && (
+            <div className="p-4">
+              <div className="grid grid-cols-2 gap-4">
+                <div className="border-2 border-purple-700 border-dashed rounded flex items-center justify-center h-48">
+                  <button className="text-white">
+                    + Add your Favorite
+                  </button>
+                </div>
+              </div>
+            </div>
+          )}
+        </div>
+
+        {/* Holder Section */}
+        <div className="mb-8 border border-white rounded-xl">
+            <div className="flex justify-between items-center p-6 bg-purple-500 rounded-xl">
+                <div
+                className="flex items-center cursor-pointer"
+                onClick={() => setIsHolderOpen(!isHolderOpen)}
+                >
+                <h2 className="text-lg font-medium mr-2">Holder</h2>
+                <span
+                    className={`transform transition-transform ${
+                    isHolderOpen ? "rotate-180" : "rotate-0"
+                    }`}
+                >
+                    ▼
+                </span>
+                </div>
+                <button className="text-sm text-white hover:underline">
+                See all
+                </button>
+            </div>
+            {isHolderOpen && (
+                <div className="p-4">
+                <div className="grid grid-cols-2 gap-4">
+                    <div className="border-2 border-purple-700 border-dashed rounded flex items-center justify-center h-48">
+                    <button className="text-white">
+                        + Add your Favorite
+                    </button>
+                    </div>
+                </div>
+                </div>
+            )}
+            </div>
+      </div>
+    </div>
+  );
+}
